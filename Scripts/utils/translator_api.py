5a3493c4bcc3f33d3f6004a12ebde8aeed99fa7d
Frank Facundo <frank.f96@hotmail.com>
2022-09-22 13:17:11 +0200

    tranlate api

---
+++ Scripts/utils/translator_api.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import googletrans
+import pprint
+from googletrans import Translator
+translator = Translator()
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+# pd.set_option('max_colwidth', 300)
+
+# how to get the supported language and their corresponing code
+# lang_df = pd.DataFrame.from_dict(googletrans.LANGUAGES,  orient='index', columns=['Language'])
+# print(lang_df.index.tolist())
+
+
+result = translator.translate(text='Hello world', dest='es', src='en')
+# pp.pprint(result.__dict__())
+print(result.text)
--- Utils/Utilities/python.md
+++ Utils/Utilities/python.md
@@ -10,6 +10,30 @@ alias python='/usr/bin/python3.4'
 python --version
 ```
 
+- Check library version
+
+```bash
+pip show packageName
+```
+
+- To check configuration of pip
+
+```bash
+pip config -v list
+```
+
+- Config file
+
+`pip.ini`
+
+```text
+[global]
+timeout = 60
+index = [link source pypi]
+index-url = [link source pypi]
+cert = [path of certificate file]
+```
+
 ## Tools
 
 Formatters: Yapf (modulable), Black (static)
