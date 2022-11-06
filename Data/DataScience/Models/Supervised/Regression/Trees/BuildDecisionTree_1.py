49e749c78577f7f6ff6dbb6a882856256f463273
Frank Facundo <frank.f96@hotmail.com>
2022-11-06 03:48:40 +0100

    WIP clean code

--- Data/DataScience/Models/Supervised/Regression/Trees/BuildDecisionTree_1.py
+++ Data/DataScience/Models/Supervised/Regression/Trees/BuildDecisionTree_1.py
@@ -5,10 +5,14 @@
 """
 import math
 from typing import List, Set, Dict, Tuple, Optional
+from collections import namedtuple
 
 INDEX_ATTR_A = 0
 INDEX_ATTR_B = 1
 INDEX_ATTR_C = 2
+
+CLASS_0 = 0
+CLASS_1 = 1
 
 
 class Node:
@@ -30,12 +34,12 @@ class Node:
         lv: :int: level ex: level root = 1
         i : :float: Information gain
         """
-        self.id = None
-        self.p = None
+        self.id: int = None
+        self.p: Node = None
         ":Node: parent node"
-        self.l = None
+        self.l: Node = None
         ":Node: left node"
-        self.r = None
+        self.r: Node = None
         ":Node: right node"
         self.a = attribute
         ":String: attribute (A or B) *if is a leave, attribute = None*"
@@ -43,17 +47,17 @@ class Node:
         ":int: if is a leave, value takes C value, it means 0 or 1"
         self.c = constraint
         ":int: constraint of the node"
-        self.cv = None
+        self.cv: List[int] = None
         ":List[int]: Number of elements for each class"
-        self.n = 0
+        self.n: int = 0
         ":int: number of records"
-        self.t = typeNode
+        self.t: str = typeNode
         ":String: type of node -> root(R), Intermediate(I), Leaf(L)"
-        self.lv = level
+        self.lv: int = level
         ":int: level ex: level root = 1"
         self.i = infoGain
         ":float: Information gain"
-        self.d = None
+        self.d: List[List[int]] = None
         ":list with values of the attributes: data"
         self.x = 0
 
@@ -71,14 +75,16 @@ class BuildDecisionTreeClass:
     """
 
     def __init__(self, file="file1.txt", minNum=2, data=None, default=0):
-        self.Attr = ["A", "B"]
+        self.Attr: List[str] = ["A", "B"]
+        ":List[str]: Features, Inputs, X"
         self.A_Domain = [0, 1, 2, 3]
         self.B_Domain = [0, 1, 2]
         self.C_Domain = [0, 1]
         self.count = 0
-        self.data = data if data else self.getTable(file)
+        self.data: List[List[int]] = data if data else self.getTable(file)
         self.default = default
-        self.dict_tree = {}
+        self.dict_tree: Dict[int, Node] = {}
+        ":Dict[int, Node]: Map the id of a Node with its Node"
         self.file = file
         self.minNum = minNum
         "minimum number of records per node"
@@ -117,34 +123,38 @@ class BuildDecisionTreeClass:
         self.dict_tree[self.root.id] = self.root
         return self.root
 
-    def countFct(self, data: List[List[int]]):
+    def countFct(self, data: List[List[int]], update=True):
         """
         Count the number of elements in each class {0, 1} 
         """
         x = 0
         for line in data:
-            if line[INDEX_ATTR_A] == 0:
+            if line[INDEX_ATTR_C] == 0:
                 x = x+1
         y = len(data)-x
-        self.count = [x, y]
-        return self.count
+        if update:
+            self.count = [x, y]
+        return [x, y]
 
-    def sameClass(self, node, data):
+    def are_rows_in_same_class(self, node: Node, data: List[List[int]]):
+        """
+        Check if all lines have the same class, if so, it creates a leaf and returns True, otherwise returns False.
+        """
         i = 0
-        while data[i][2] == data[0][2]:
+        while data[i][INDEX_ATTR_C] == data[0][INDEX_ATTR_C]:
             i = i+1
             if i == len(data):
                 break
 
         if i == len(data):
             node.t = "Leaf"
-            node.v = data[0][2]
+            node.v = data[0][INDEX_ATTR_C]
 
             c = [0, 0]
             for line in data:
-                if line[2] == 0:
+                if line[INDEX_ATTR_C] == CLASS_0:
                     c[0] = c[0]+1
-                elif line[2] == 1:
+                elif line[INDEX_ATTR_C] == CLASS_1:
                     c[1] = c[1]+1
             node.cv = c
 
@@ -156,12 +166,13 @@ class BuildDecisionTreeClass:
             self.Result += "\nid " + str(node.id)
             self.Result += "\nC = " + str(node.v) + "\n"
 
-            return node
+            return True
         else:
-            return 0
+            return False
 
-    def verifyMinNum(self, node, data):
+    def is_min_num(self, node, data):
         """
+        Check if data has the minimum number of rows allowed.
         data : (List) list of record
         """
         if len(data) < self.minNum:
@@ -178,20 +189,20 @@ class BuildDecisionTreeClass:
 
             c = [0, 0]
             for line in data:
-                if line[2] == 0:
+                if line[INDEX_ATTR_C] == CLASS_0:
                     c[0] = c[0]+1
-                elif line[2] == 1:
+                elif line[INDEX_ATTR_C] == CLASS_1:
                     c[1] = c[1]+1
             node.cv = c
 
             self.Process += "\nRecords are less than minNum so it finish!"
 
-            return node
+            return True
 
         else:
-            return 0
+            return False
 
-    def emptyAttr(self, node, Attr, data):
+    def is_attr_empty(self, node: Node, Attr, data):
 
         self.countFct(data)
         if (len(Attr) == 0):
@@ -205,15 +216,15 @@ class BuildDecisionTreeClass:
 
             c = [0, 0]
             for line in data:
-                if line[2] == 0:
+                if line[INDEX_ATTR_C] == 0:
                     c[0] = c[0]+1
-                elif line[2] == 1:
+                elif line[INDEX_ATTR_C] == 1:
                     c[1] = c[1]+1
             node.cv = c
 
-            return node
+            return True
         else:
-            return
+            return False
 
     def split(self, listAttribute, data):
 
@@ -221,18 +232,18 @@ class BuildDecisionTreeClass:
         maxAttribute = None
 
         for Attr in listAttribute:
-            ResultSplitAttr = self.splitAttr(Attr, data)
-            if (ResultSplitAttr[1] > maxGain):
-                maxGain = ResultSplitAttr[1]
+            best_split = self.splitAttr(Attr, data)
+            if (best_split.gain > maxGain):
+                maxGain = best_split.gain
                 maxAttribute = Attr
-                maxS = ResultSplitAttr
-                C_values = ResultSplitAttr[2]
+                maxS = best_split
+                C_values = best_split.nb_clases_by_leaf
 
-        feature = [maxAttribute, maxS[0], maxGain, C_values]
+        feature = [maxAttribute, maxS.best_split, maxGain, C_values]
 
         return feature
 
-    def splitAttr(self, Attribute, data):
+    def splitAttr(self, Attribute, data: List[List[int]]):
         splitA = [[[0], [1, 2, 3]], [[0, 1], [2, 3]],
                   [[0, 1, 2], [3]], [[0, 1, 2, 3], []]]
         splitB = [[[0], [1, 2]], [[0, 1], [2]], [[0, 1, 2], []]]
@@ -243,71 +254,52 @@ class BuildDecisionTreeClass:
         p = self.countFct(data)
         i1 = [0, 0]
         i2 = [0, 0]
-        if (Attribute == "A"):
-            for i in splitA:
-                i1 = [0, 0]
-                i2 = [0, 0]
-                for line in data:
-                    if (line[0] in i[0]):
-                        if line[2] == 0:
-                            i1[0] = i1[0]+1
-                        elif line[2] == 1:
-                            i1[1] = i1[1]+1
-                    else:
+        map_attr_split = {"A": splitA, "B": splitB}
 
-                        if line[2] == 0:
-                            i2[0] = i2[0]+1
-                        elif line[2] == 1:
-                            i2[1] = i2[1]+1
+        for i in map_attr_split[Attribute]:
+            # Count number element for each class (O or 1) for left leaf
+            i1 = [0, 0]
+            # Count number element for each class (O or 1) for right leaf
+            i2 = [0, 0]
+            for line in data:
+                if (line[INDEX_ATTR_A] in i[0]):
+                    if line[INDEX_ATTR_C] == CLASS_0:
+                        i1[0] = i1[0]+1
+                    elif line[INDEX_ATTR_C] == CLASS_1:
+                        i1[1] = i1[1]+1
+                else:
+                    if line[INDEX_ATTR_C] == CLASS_0:
+                        i2[0] = i2[0]+1
+                    elif line[INDEX_ATTR_C] == CLASS_1:
+                        i2[1] = i2[1]+1
 
-                currentGain = self.informationGain(p, i1, i2)
-                if (currentGain > gain):
-                    bestSplit = i
-                    gain = currentGain
-                    c_values = [i1, i2]
-        elif (Attribute == "B"):
-            for i in splitB:
-                i1 = [0, 0]
-                i2 = [0, 0]
-                for line in data:
-                    if (line[0] in i[0]):
-                        if line[2] == 0:
-                            i1[0] = i1[0]+1
-                        elif line[2] == 1:
-                            i1[1] = i1[1]+1
-                    else:
-
-                        if line[2] == 0:
-                            i2[0] = i2[0]+1
-                        elif line[2] == 1:
-                            i2[1] = i2[1]+1
-
-                currentGain = self.informationGain(p, i1, i2)
-                if (currentGain > gain):
-                    bestSplit = i
-                    gain = currentGain
-                    c_values = [i1, i2]
-        return [bestSplit, gain, c_values]
+            currentGain = self.informationGain(p, i1, i2)
+            if (currentGain > gain):
+                bestSplit = i
+                gain = currentGain
+                c_values = [i1, i2]
+        BestSplit = namedtuple(
+            'BestSplit', ["best_split", "gain", "nb_clases_by_leaf"])
+        best_split = BestSplit(bestSplit, gain, c_values)
+        return best_split
 
     def entropy(self, node):
         n1 = node[0]
         n2 = node[1]
         n = n1 + n2
 
-        if n1 == 0:
-            m1 = 0
-        else:
-            m1 = math.log2(n1/n)
+        prob_n1 = n1/n
+        prob_n2 = n2/n
+        log_prob_n1 = 0 if prob_n1 == 0 else math.log2(prob_n1)
+        log_prob_n2 = 0 if prob_n2 == 0 else math.log2(prob_n2)
 
-        if n2 == 0:
-            m2 = 0
-        else:
-            m2 = math.log2(n2/n)
-
-        entropy = -((n1/n) * m1 + (n2/n) * m2)
+        entropy = -(prob_n1*log_prob_n1 + prob_n2*log_prob_n2)
         return entropy
 
     def informationGain(self, p, i_1, i_2):
+        """
+        p : Count the number of elements in each class {0, 1}
+        """
         if (i_2 != [0, 0] and i_1 != [0, 0]):
             n1 = i_1[0] + i_1[1]
             n2 = i_2[0] + i_2[1]
@@ -360,23 +352,23 @@ class BuildDecisionTreeClass:
             reste = reste - index*powers
         return listPath
 
-    def build(self, data, listAttribute, node):
+    def build(self, data: List[List[int]], listAttribute, node: Node):
 
         self.Process += "\n\n******************************One iteration******************************\n"
 
         node.d = data
 
-        resultSameClass = self.sameClass(node, data)
-        if resultSameClass != 0:
-            return resultSameClass
+        are_rows_in_same_class = self.are_rows_in_same_class(node, data)
+        if are_rows_in_same_class:
+            return
 
-        resultMinNum = self.verifyMinNum(node, data)
-        if resultMinNum != 0:
-            return resultMinNum
+        is_min_num = self.is_min_num(node, data)
+        if is_min_num:
+            return
 
-        resultEmptyAttr = self.emptyAttr(node, listAttribute, data)
-        if resultEmptyAttr != None:
-            return resultEmptyAttr
+        is_attr_empty = self.is_attr_empty(node, listAttribute, data)
+        if is_attr_empty:
+            return
 
         # Split returns the attribute taken, the values of attributes for the left and right side and the information gain
         feature = self.split(listAttribute, data)
