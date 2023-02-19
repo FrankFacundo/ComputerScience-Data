cd59393f49115680da8b7bedb8bd3022a2683c19
Frank Facundo <frank.f96@hotmail.com>
2023-02-19 21:12:10 +0100

    update chatgpt api

--- Data/DataScience/Models/ByFunction/LLM/chatGPT_API.py
+++ Data/DataScience/Models/ByFunction/LLM/chatGPT_API.py
@@ -1,12 +1,19 @@
 import os
+import sys
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+s1 = sys.argv[1]
+print(s1)
+
 response = openai.Completion.create(
-  model="text-chat-davinci-002-20230126",
-  prompt="Which optimization algorithm is the best to solve SVM?",
-  max_tokens=4000,
+    engine="code-davinci-002",
+    prompt=s1,
+    temperature=1,
+    max_tokens=50,
+    top_p=1,
+    # stop=["\n"]
 )
 
 print(response['choices'][0]['text'])
