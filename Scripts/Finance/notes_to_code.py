2e9a9ec15689a3e9ddeec9ae1b97ceee2bd317f7
Frank Facundo <frank.f96@hotmail.com>
2023-10-08 22:20:47 +0200

    notes finance

---
+++ Scripts/Finance/notes_to_code.py
@@ -0,0 +1,10 @@
+# PNL Explain from Taylor of Black-Scholes 
+# http://faculty.baruch.cuny.edu/lwu/4750/4750Lec7.pdf
+# http://www.pnlexplained.com/PEP_PnL_Explained_FAQ.html
+# https://bookdown.org/maxime_debellefroid/MyBook/the-greeks.html
+# http://media.wix.com/ugd/c4ff5c_ba17141422d44ba99daf19ee2b931544.pdf
+# https://quant.stackexchange.com/questions/31068/taylor-expansion-of-pnl
+
+#---
+
+# Master: https://finance.math.upmc.fr/enseignements/enseignement/
