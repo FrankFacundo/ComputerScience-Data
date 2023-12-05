06970fcb178f47cd05768ab42201e795321774b1
Frank Facundo <frank.f96@hotmail.com>
2023-12-05 23:34:08 +0100

    add docker pyspark offline

---
+++ Data/DataEngineering/Docker/images/pyspark/download_java.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Java 8 : https://jdk.java.net/java-se-ri/8-MR5
+# Chosen Java 8 because is the version that Anaconda installs.
+
+curl -O https://download.java.net/openjdk/jdk8u43/ri/openjdk-8u43-linux-x64.tar.gz
