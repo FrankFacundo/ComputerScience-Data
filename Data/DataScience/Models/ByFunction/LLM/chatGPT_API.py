dc9b9f376b724cc5c39302953de848030653863d
Frank Facundo <frank.f96@hotmail.com>
2024-03-31 21:45:27 +0200

    update chatgpt script

--- Data/DataScience/Models/ByFunction/LLM/chatGPT_API.py
+++ Data/DataScience/Models/ByFunction/LLM/chatGPT_API.py
@@ -1,35 +1,27 @@
 import os
-import sys
-import openai
 import time
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+from openai import OpenAI
 
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 # s1 = sys.argv[1]
 s1 = "quién eres?"
 print(s1)
 
 start = time.time()
 
-response = openai.ChatCompletion.create(
-  # max_tokens=85,
-  model="gpt-3.5-turbo",
-  messages=[
+chat_completion = client.chat.completions.create(
+    messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": s1},
-    ]
+    ],
+    model="gpt-3.5-turbo",
 )
+
 
 print(time.time() - start)
 
-print(response['choices'][0]['message']['content'])
-
-response2 = response.create(
-  messages=[
-        {"role": "user", "content": "Alguna informacion extra que quieras agregar?"}
-    ]
-)
-
-print(response2['choices'][0]['message']['content'])
-
-
+print(chat_completion.choices[0].message.content)
