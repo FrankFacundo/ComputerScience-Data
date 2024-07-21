634d4b849906bb1c8618e5eda199b649b5c8260e
Frank Facundo <frank.f96@hotmail.com>
2024-07-22 00:00:45 +0200

    intern vit

---
+++ Data/DataScience/Models/ByFunction/VLLM/intern_vit.py
@@ -0,0 +1,33 @@
+import os
+
+import torch
+from PIL import Image
+from transformers import AutoModel, CLIPImageProcessor
+
+local_path = "your_path"
+local_path = "/media/frank/My Passport/DL_models"
+hf_path = "OpenGVLab/InternViT-6B-448px-V1-5"
+
+
+model = (
+    AutoModel.from_pretrained(
+        local_path,
+        torch_dtype=torch.bfloat16,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True,
+    )
+    .cuda()
+    .eval()
+)
+
+filepath = os.path.join(os.path.dirname(__file__), "examples/image1.png")
+
+image = Image.open(filepath).convert("RGB")
+
+image_processor = CLIPImageProcessor.from_pretrained(local_path)
+
+pixel_values = image_processor(images=image, return_tensors="pt").pixel_values
+pixel_values = pixel_values.to(torch.bfloat16).cuda()
+
+outputs = model(pixel_values)
+print(outputs)
