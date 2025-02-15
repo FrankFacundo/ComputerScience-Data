26ea0d8b189355d10c124ac5934d1073b2fc4081
Frank Facundo <frank.f96@hotmail.com>
2025-02-15 11:55:22 +0100

    update script to download drive files

--- Scripts/utils/drive_download.py
+++ Scripts/utils/drive_download.py
@@ -1,52 +1,96 @@
-#Reference: https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
+# Reference: https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
 
 import argparse
+import re
+
 import requests
 from tqdm import tqdm
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Download a file from Google Drive using its shareable link."
+        "The file name is automatically extracted from the response headers."
+    )
+    parser.add_argument(
+        "-l", "--link", type=str, required=True, help="Google Drive shareable link"
+    )
+    return parser.parse_args()
 
-    parser.add_argument('-i', '--file_id', type=str)
-    parser.add_argument('-d', '--destination', type=str)
 
-    args = parser.parse_args()
-    return args
-
-def download_file_from_google_drive(id, destination):
+def download_file_from_google_drive_file_id(id):
     URL = "https://docs.google.com/uc?export=download"
 
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params={"id": id}, stream=True)
     token = get_confirm_token(response)
 
     if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
 
-    save_response_content(response, destination)    
+    destination = get_file_name(response)
+    save_response_content(response, destination)
+
 
 def get_confirm_token(response):
     for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
+        if key.startswith("download_warning"):
             return value
 
     return None
+
+
+def get_file_name(response):
+    """
+    Extracts file name from the Content-Disposition header.
+    Returns a default name if the header is not present.
+    """
+    cd = response.headers.get("Content-Disposition")
+    if cd:
+        # Example header: 'attachment; filename="example.pdf"'
+        fname = re.findall('filename="(.+)"', cd)
+        if fname:
+            return fname[0]
+    return "downloaded_file"
+
 
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
 
     with open(destination, "wb") as f:
-        for chunk in tqdm(response.iter_content(CHUNK_SIZE)):
-            if chunk: # filter out keep-alive new chunks
+        for chunk in tqdm(
+            response.iter_content(CHUNK_SIZE), desc="Downloading", unit="chunk"
+        ):
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
 
-if __name__ == "__main__":
-    # Command example:
-    #   python drive_download.py -i 1A5H4fZ38NxWLyX7wedINghC1iPRb9LzD -d online_retail_II.xlsx
-    
-    args = parse_args()
-    download_file_from_google_drive(args.file_id, args.destination)
 
+def extract_file_id(file_input):
+    """
+    Extracts the file ID from a Google Drive URL. If file_input is not a URL,
+    it is assumed to be a file ID and returned as is.
+
+    Example URL: https://drive.google.com/file/d/1IMiuHsiVvna_iN7vdZBMZy8DviFnrvYY/view?usp=sharing
+    """
+    if file_input.startswith("http"):
+        match = re.search(r"/d/([^/]+)", file_input)
+        if match:
+            return match.group(1)
+        else:
+            raise ValueError(
+                "Invalid Google Drive link format. Could not extract file ID."
+            )
+    else:
+        return file_input
+
+
+def download_file_from_google_drive(link):
+    file_id = extract_file_id(link)
+    download_file_from_google_drive_file_id(file_id)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    download_file_from_google_drive(args.link)
