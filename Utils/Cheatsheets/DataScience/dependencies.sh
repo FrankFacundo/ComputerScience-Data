818410e6a6332cdb890177a766029e176552a518
Frank Facundo <frank.f96@hotmail.com>
2025-10-05 14:03:22 +0200

    update ds cheatsheet

--- Utils/Cheatsheets/DataScience/dependencies.sh
+++ Utils/Cheatsheets/DataScience/dependencies.sh
@@ -1,2 +1,8 @@
-apt install texlive-science texlive-lang-portuguese texlive-extra-utils latexmk texlive-latex-extra
 # With VSCode, first install "LaTeX Workshop" then it can be compiled with the button play, the same when running a Python script. 
+apt install \
+    texlive-science \
+    texlive-lang-portuguese \
+    texlive-extra-utils \
+    latexmk \
+    texlive-latex-extra \
+    texlive-fonts-extra
