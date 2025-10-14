81220fd33f3256043024be44733042bd3dbe7b56
Frank Facundo <frank.f96@hotmail.com>
2025-10-14 17:49:23 +0200

    update copy commands

--- Data/DataEngineering/Docker/images/copy/entrypoint.sh
+++ Data/DataEngineering/Docker/images/copy/entrypoint.sh
@@ -5,5 +5,6 @@ set -x
 docker build -t files .
 docker run --rm files ls -alh /files
 docker tag files frankfacundo/files
+# docker run -it --entrypoint /bin/sh frankfacundo/files
 # docker login -u frankfacundo
 docker push frankfacundo/files
